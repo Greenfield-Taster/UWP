46a870778d183ea8a365a5d5dccd36e72c6845ec
Asya Gorbatchova <gorbatchovaasa123@gmail.com>
2023-01-08 23:22:57 +0200

    Added print to the main page

--- App1/MainPage.xaml.cs
+++ App1/MainPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
+using static System.Net.Mime.MediaTypeNames;
 
 // Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
 
@@ -15,14 +17,32 @@ namespace App1
         {
             this.InitializeComponent();
 
-            var BI = new BitmapImage(new Uri("ms-appx:///earth.gif"));
+            //var BI = new BitmapImage(new Uri("ms-appx:///earth.gif"));
 
-            LIB14393.AnimatedGif.SetAutoPalyOff(BI);
+            //LIB14393.AnimatedGif.SetAutoPalyOff(BI);
 
-            var Image = new Image();
-            Image.Source = BI;
-            this.Content = Image;
+            //var Image = new Image();
+            //Image.Source = BI;
+            //this.Content = Image;
+
+            /*********************************************************/
+
+            var G = new Grid()
+            {
+                Background = new SolidColorBrush(Windows.UI.Colors.Blue),
+            };
+            var TB = new TextBlock()
+            {
+                FontSize = 50,
+            };
+            var R = Windows.UI.Xaml.Documents.Run()
+                {
+                    Text = "Hello, World!"
+                };
+           
+            TB.Inlines.Add(R);
+            G.Children.Add(TB);
+            this.Content = G;
         }
-
     }
 }
